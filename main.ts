5831589da6964451568e3e83d5c0239665d2af15
Adam Coddington <me@adamcoddington.net>
2020-10-29 20:12:48 -0700

    Restructuring document; updating changelog.

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import {
   FileView,
   MarkdownView,
   Plugin,
+  TAbstractFile,
   TFile,
 } from "obsidian";
 import * as http from "http";
@@ -16,26 +17,37 @@ export default class ObsidianPrint extends Plugin {
 
   server: http.Server;
 
-  async requestListener(
-    request: http.IncomingMessage,
-    response: http.ServerResponse
-  ) {
-    const parsedUrl = new url.URL(`http://127.0.0.1${request.url}`);
-    const location = decodeURI(parsedUrl.pathname).slice(1);
+  async writeMarkdownPreviewResponse(response: http.ServerResponse) {
     const activeLeaf = this.app.workspace.activeLeaf;
     const view = activeLeaf.view as MarkdownView;
     const adapter = this.app.vault.adapter as FileSystemAdapter;
     const basePath = adapter.getBasePath();
     const cssPath = basePath + "/obsidian.css";
+    let body = view.previewMode.containerEl.innerHTML;
 
-    const root = this.app.vault.getRoot();
+    const pathReplacements = [
+      `app://local/${encodeURIComponent(adapter.getBasePath())}%2F`,
+    ];
+    for (const replacement of pathReplacements) {
+      body = body.replace(replacement, "");
+    }
 
-    const matchingFiles = root.children.filter((fileData: TFile) => {
-      return (
-        fileData.path == location ||
-        (fileData.basename == location && fileData.extension == "md")
-      );
-    });
+    response.writeHead(200, "OK", { "Content-type": "text/html" });
+    response.write("<!DOCTYPE html><html><head><meta charset='utf-8' />");
+    if (fs.existsSync(cssPath)) {
+      response.write("<style type='text/css'>");
+      response.write(fs.readFileSync(cssPath));
+      response.write("</style>");
+    }
+    response.write("</head><body>");
+    response.write(body);
+    response.write("</body></html>");
+    response.end();
+    return;
+  }
+
+  async writeRawFileResponse(filename: string, response: http.ServerResponse) {
+    const matchingFiles = this.getMatchingFileObjectsForRoute(filename);
 
     if (matchingFiles.length == 0) {
       response.writeHead(404, "Not Found");
@@ -48,37 +60,35 @@ export default class ObsidianPrint extends Plugin {
       response.end();
       return;
     }
+
     const matchingFile = matchingFiles[0] as TFile;
+    const data = await this.app.vault.readBinary(matchingFile);
 
-    if (matchingFile.extension == "md") {
-      let body = view.previewMode.containerEl.innerHTML;
+    response.writeHead(200, "OK", {
+      "Content-Type": "application/octet-stream",
+    });
+    response.write(Buffer.from(data));
+    response.end();
+  }
 
-      const pathReplacements = [
-        `app://local/${encodeURIComponent(adapter.getBasePath())}%2F`,
-      ];
-      for (const replacement of pathReplacements) {
-        body = body.replace(replacement, "");
-      }
+  getMatchingFileObjectsForRoute(filename: string): TAbstractFile[] {
+    const root = this.app.vault.getRoot();
+    return root.children.filter((fileData: TFile) => {
+      return fileData.path == filename;
+    });
+  }
 
-      response.writeHead(200, "OK", { "Content-type": "text/html" });
-      response.write("<!DOCTYPE html><html><head><meta charset='utf-8' />");
-      if (fs.existsSync(cssPath)) {
-        response.write("<style type='text/css'>");
-        response.write(fs.readFileSync(cssPath));
-        response.write("</style>");
-      }
-      response.write("</head><body>");
-      response.write(body);
-      response.write("</body></html>");
-      response.end();
+  async requestListener(
+    request: http.IncomingMessage,
+    response: http.ServerResponse
+  ) {
+    const parsedUrl = new url.URL(`http://127.0.0.1${request.url}`);
+    const location = decodeURI(parsedUrl.pathname).slice(1);
+
+    if (location == "") {
+      this.writeMarkdownPreviewResponse(response);
     } else {
-      const data = await this.app.vault.readBinary(matchingFile);
-
-      response.writeHead(200, "OK", {
-        "Content-Type": "application/octet-stream",
-      });
-      response.write(Buffer.from(data));
-      response.end();
+      this.writeRawFileResponse(location, response);
     }
   }
 
@@ -94,7 +104,7 @@ export default class ObsidianPrint extends Plugin {
         const activeView = activeLeaf.view as FileView;
         const filename = activeView.file.path;
 
-        open(`http://127.0.0.1:${this.port}/${filename}`);
+        open(`http://127.0.0.1:${this.port}/`);
       },
     });
     this.addCommand({
